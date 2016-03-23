41b83fc7218e42230ff3598a7228f2e242e53355
Shaif821 <Shaif.bhaggoe@gmail.com>
2016-03-23 09:04:44 +0100

    Code aangepast

--- tabPanelen.js
+++ tabPanelen.js
@@ -1,22 +1,20 @@
-$('.panelen').each( function() {
-    var dezeKnop = $(this);
-    var actieveKnop = dezeKnop.find('li.actief');
-    var link = actieveKnop.find('a');
-    var actiefPaneel = $(link.attr('href'));
+$('.tabpanelen').each( function() {
+    var mijnTab = $(this);
+    var actieveButton = mijnTab.find('li.actief');
+    var link = actieveButton.find('a');
+    var actiefTab = $(link.attr('href'));
     
-    dezeKnop.on('click' , 'a' , function(e) {
+    mijnTab.on('click' , 'a' , function(e) {
         e.preventDefault();
         var link = $(this);
-//        alert(link.attr('href'));
         var beoogdID = this.hash;
-//        alert(beoogdID);
         
         if(beoogdID && !link.parent().is('.actief')){
-            actiefPaneel.removeClass('actief');
-            actieveKnop.removeClass('actief');
+            actiefTab.removeClass('actief');
+            actieveButton.removeClass('actief');
             
-            actiefPaneel = $(beoogdID).addClass('actief');
-            actieveKnop = link.parent().addClass('actief');
+            actiefTab = $(beoogdID).addClass('actief');
+            actieveButton = link.parent().addClass('actief');
         }
     })
     
